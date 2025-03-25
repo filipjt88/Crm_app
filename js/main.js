27ce6852fc0bab10f51763573ddfa75690d2c7c2
filipjt88 <filip88ks@gmail.com>
2025-03-25 23:38:40 +0100

    save

---
+++ js/main.js
@@ -0,0 +1,22 @@
+function loadClients() {
+    fetch('get_clients.php')
+        .then(response => response.json())
+        .then(data => {
+            let tableBody = document.getElementById("clientsTable");
+            tableBody.innerHTML = '';
+            data.forEach(client => {
+                let row = `
+                    <tr>
+                        <td>${client.first_name}</td>
+                        <td>${client.last_name}</td>
+                        <td>${client.email}</td>
+                        <td>${client.phone}</td>
+                        <td>${client.notes}</td>
+                    </tr>
+                `;
+                tableBody.innerHTML += row;
+            });
+        })
+        .catch(error => console.error("Greska pri ucitavanju podataka!", error));
+}
+window.onload = loadClients;
