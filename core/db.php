dc53c68d290b24027796d650d750511a21f28875
filipjt88 <filip88ks@gmail.com>
2025-03-28 23:01:39 +0100

    save

--- core/db.php
+++ core/db.php
@@ -5,13 +5,13 @@ $username = 'root';
 $password = '';
 $charset = 'utf8mb4';
 
-// try {
-//     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-// } catch (PDOException $e) {
-//     die("Niste povezani sa bazom podataka!" . $e->getMessage());
-// }
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Niste povezani sa bazom podataka!" . $e->getMessage());
+}
 
 $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset";
 $options = [
