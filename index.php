b0ad0325c2e6de6e6b2d4ceda33944ac963c556f
filipjt88 <filip88ks@gmail.com>
2025-03-30 21:27:54 +0200

    save

--- index.php
+++ index.php
@@ -1,38 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+include 'parts/head.php';
+include 'parts/sidebar.php';
+include 'parts/footer.php';
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Clients list</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/main.css">
-</head>
+?>
 
-<body>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-9 offset-1 mt-5">
-                <h1 class="text-center">All clients</h1>
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>First name:</th>
-                            <th>Last name:</th>
-                            <th>Email:</th>
-                            <th>Phone:</th>
-                            <th>Notes:</th>
-                            <th class="text-center" colspan="2">Action:</th>
-                        </tr>
-                    </thead>
-                    <tbody id="clientsTable"></tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-    <script src="./js/main.js"></script>
-</body>
 
-</html>
---
+++ parts/sidebar.php
@@ -0,0 +1,24 @@
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9 offset-1 mt-5">
+                <h1 class="text-center">All clients</h1>
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>First name:</th>
+                            <th>Last name:</th>
+                            <th>Email:</th>
+                            <th>Phone:</th>
+                            <th>Notes:</th>
+                            <th class="text-center" colspan="2">Action:</th>
+                        </tr>
+                    </thead>
+                    <tbody id="clientsTable"></tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <script src="./js/main.js"></script>
+</body>
