d105e1afe82e1e8033f9203b44baa3eaaddcc9c9
filipjt88 <filip88ks@gmail.com>
2025-04-11 22:36:30 +0200

    update

--- controller/add_client.php
+++ controller/add_client.php
@@ -8,9 +8,11 @@ if (!isset($_SESSION['user_id'])) {
 
 <?php include '../parts/top.php'; ?>
 
-<div class="container">
-        <h2>Dodaj novog klijenta</h2>
-        <form action="store_client.php" method="POST">
+<div class="container mt-5">
+        <h2 class="text-center">Dodaj novog klijenta</h2>
+        <div class="row">
+            <div class="col-md-6 offset-3">
+            <form action="store_client.php" method="POST">
             <div class="mb-3">
                 <label for="name" class="form-label">Ime i prezime *</label>
                 <input type="text" name="name" id="name" class="form-control" required>
@@ -34,6 +36,8 @@ if (!isset($_SESSION['user_id'])) {
             <button type="submit" class="btn btn-success">Sačuvaj</button>
             <a href="clients.php" class="btn btn-secondary">Nazad</a>
         </form>
+            </div>
+        </div>
     </div>
 
 <?php include '../parts/bottom.php'; ?>
