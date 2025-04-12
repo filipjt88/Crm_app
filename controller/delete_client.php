3768ec9086513e50379132013ef36c84a65c947b
filipjt88 <filip88ks@gmail.com>
2025-04-12 22:39:19 +0200

    update

--- controller/delete_client.php
+++ controller/delete_client.php
@@ -1,8 +1,14 @@
 <?php
 
-require '../core/db.php';
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header('Location: ../controller/login.php');
+    exit;
+}
 
 require '../parts/top.php';
+
+
 
 
 require '../parts/bottom.php';
