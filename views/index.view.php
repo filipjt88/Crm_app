409fd092911018bae30bf3b7c01c86b54a70ccdc
filipjt88 <filip88ks@gmail.com>
2025-04-16 10:13:00 +0200

    save

--- views/index.view.php
+++ views/index.view.php
@@ -10,11 +10,11 @@ if (!isset($_SESSION['user_id'])) {
     <div class="container text-center">
         <div class="row">
             <div class="col-md-6 offset-3 mt-5">
-                <sectio class="user">
+                <section class="user">
                 <h1>Dobrodošao, <?php echo htmlspecialchars($_SESSION['user_name']); ?>!</h1>
         <span>Uloga: <?php echo htmlspecialchars($_SESSION['user_role']); ?></span>
         <a href="../controller/logout.php" class="btn btn-danger">Odjava</a>
-                </sectio>
+                </section>
             </div>
         </div>
     </div>
