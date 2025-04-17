40a63d7a1bbe360d68624879b0a68ace579d3a24
filipjt88 <filip88ks@gmail.com>
2025-04-17 22:11:08 +0200

    update client

--- views/clients.view.php
+++ views/clients.view.php
@@ -1,9 +1,13 @@
 <?php
+session_start(); 
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
 
-session_start();
+
 require_once '../core/db.php';
 
-if(!isset($_SESSION('user_id'))) {
+if (!isset($_SESSION['user_id'])) {
     header('Location: ../controller/login.php');
     exit;
 }
