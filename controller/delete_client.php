e78002781e8fe7e4003453c6072bc51b68ccfadd
filipjt88 <filip88ks@gmail.com>
2025-04-17 22:03:56 +0200

    save delete client

--- controller/delete_client.php
+++ controller/delete_client.php
@@ -8,6 +8,23 @@ if (!isset($_SESSION['user_id'])) {
 
 require '../parts/top.php';
 
+if (!isset($_GET['id'])) {
+    die('Client ID is missing.');
+}
+
+$clientId = $_GET['id'];
+
+// Priprema i izvršavanje DELETE upita
+try {
+    $stmt = $pdo->prepare("DELETE FROM clients WHERE id = :id");
+    $stmt->execute(['id' => $clientId]);
+
+    // Redirekcija nazad na listu klijenata
+    header("Location: clients_list.php?msg=deleted");
+    exit;
+} catch (PDOException $e) {
+    echo "Error deleting client: " . $e->getMessage();
+}
 
 
 
