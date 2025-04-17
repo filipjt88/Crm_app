77585badc33693c7d208cb12f90fb3572c8dec0b
filipjt88 <filip88ks@gmail.com>
2025-04-17 22:34:57 +0200

    save

--- controller/delete_client.php
+++ controller/delete_client.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once '../core/db.php';
 session_start();
 if (!isset($_SESSION['user_id'])) {
     header('Location: ../controller/login.php');
@@ -8,24 +8,23 @@ if (!isset($_SESSION['user_id'])) {
 
 require '../parts/top.php';
 
-if (!isset($_GET['user_id'])) {
-    die('Client ID is missing.');
+if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
+    die('Client ID is missing or invalid.');
 }
 
-$user_id = $_GET['user_id'];
+$user_id = $_GET['id'];
 
-// Priprema za izvršavanje DELETE upita
 try {
+    // Brisanje klijenta po ID-u
     $stmt = $pdo->prepare("DELETE FROM clients WHERE id = :id");
     $stmt->execute(['id' => $user_id]);
 
-    // Redirekcija nazad na listu klijenata
-    header("Location: clients_list.php?msg=deleted");
+    // Redirekcija nakon uspešnog brisanja
+    header("Location: store_client.php?msg=deleted");
     exit;
 } catch (PDOException $e) {
     echo "Error deleting client: " . $e->getMessage();
 }
-
 
 
 require '../parts/bottom.php';
