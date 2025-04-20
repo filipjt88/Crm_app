332e218d1f031e5e2529ff5e567c268f92716357
filipjt88 <filip88ks@gmail.com>
2025-04-20 22:17:04 +0200

    update register

--- views/register.view.php
+++ views/register.view.php
@@ -1,14 +1,7 @@
 <?php require '../core/db.php'; ?>
-<!DOCTYPE html>
-<html lang="sr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registracija</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body class="d-flex align-items-center justify-content-center vh-100">
-    <div class="container">
+<?php require '../parts/top.php'; ?>
+<?php require '../parts/navbar.php'; ?>
+    <div class="container d-flex align-items-center justify-content-center vh-100">
         <div class="row justify-content-center">
             <div class="col-md-6">
                 <div class="card shadow-lg p-4">
@@ -39,5 +32,5 @@
             </div>
         </div>
     </div>
-</body>
-</html>
+<?php require '../parts/bottom.php'; ?>
+
